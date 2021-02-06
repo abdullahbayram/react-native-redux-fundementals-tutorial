6580b7b1bd3a2a5048b64304fbe69632d004ce47
abayram <38274063+abdullahbayram@users.noreply.github.com>
2021-02-06 21:29:22 +0300

    Different from tutorial redux-logger middleware is added

--- src/store.js
+++ src/store.js
@@ -1,12 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducer'
-import { print1, print2, print3 } from './exampleAddons/middleware'
+import logger from 'redux-logger'
 
-
-const middlewareEnhancer = applyMiddleware(print1, print2, print3)
-
-// Pass enhancer as the second arg, since there's no preloadedState
-const store = createStore(rootReducer, middlewareEnhancer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 export default store
 
