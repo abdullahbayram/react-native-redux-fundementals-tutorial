9f5cfa7f7f6e05b8d9769e95a0b884dbee1a8d71
abayram <38274063+abdullahbayram@users.noreply.github.com>
2021-02-06 18:39:50 +0300

    Add and use sayHi store enhancer

--- App.js
+++ App.js
@@ -6,36 +6,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import store from './src/store'
 
-// Log the initial state
-console.log('Initial state: ', store.getState())
-// {todos: [....], filters: {status, colors}}
-
-// Every time the state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-const unsubscribe = store.subscribe(() =>
-    console.log('State after dispatch: ', store.getState())
-)
-
-// Now, dispatch some actions
-
+console.log('Dispatching action')
 store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers' })
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores' })
-
-store.dispatch({ type: 'todos/todoToggled', payload: 0 })
-store.dispatch({ type: 'todos/todoToggled', payload: 1 })
-
-store.dispatch({ type: 'filters/statusFilterChanged', payload: 'Completed' })
-
-store.dispatch({
-  type: 'filters/colorFilterChanged',
-  payload: { color: 'red', changeType: 'selected' },
-})
-
-// Stop listening to state updates
-unsubscribe()
-
-// Dispatch one more action to see what happens
+console.log('Dispatch complete')
 
 export default function App() {
   return (
