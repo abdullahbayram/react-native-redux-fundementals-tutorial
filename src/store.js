ce679b7d59653ac2a2963e2b304675fddca3bfec
abayram <38274063+abdullahbayram@users.noreply.github.com>
2021-02-06 21:21:47 +0300

    use react-native-debugger instead of redux-devtools-extension

--- src/store.js
+++ src/store.js
@@ -9,3 +9,8 @@ const middlewareEnhancer = applyMiddleware(print1, print2, print3)
 const store = createStore(rootReducer, middlewareEnhancer)
 
 export default store
+
+// Using redux-devtools-extension is different for React Native
+// https://stackoverflow.com/questions/52121683/use-redux-devtools-extension-with-react-native-with-chrome
+// This link have two possibilities to use redux-devtools-extension for React Native. I chose to use https://github.com/jhen0409/react-native-debugger.
+// You can install and use react-native-debugger easily.
